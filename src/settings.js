8733b1c78ee670e9a1b1837a86052f45b6dbc880
Dhawal Majithia <deviolxd@icloud.com>
2019-01-09 15:26:04 -0800

    First

---
+++ src/settings.js
@@ -0,0 +1,8 @@
+
+export const url = 'http://192.168.1.8:58788/'
+export const hosts = 'tools.fouad.org'
+export const iframeHosts = 'https://tools.fouad.org'
+
+export default {
+  hosts, iframeHosts, url
+}
