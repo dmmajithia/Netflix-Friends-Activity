a7bd52c239de60c370f5354857c3323828fbc4a8
Rosalia Dupont <rodupontayala@ucdavis.edu>
2019-01-09 18:12:44 -0800

    removed files and added manifest

--- src/settings.js
+++
@@ -1,8 +0,0 @@
-
-export const url = 'http://192.168.1.8:58788/'
-export const hosts = 'tools.fouad.org'
-export const iframeHosts = 'https://tools.fouad.org'
-
-export default {
-  hosts, iframeHosts, url
-}
