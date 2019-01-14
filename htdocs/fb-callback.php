04b2bb03e047d2c3cfbd34c83dc66cf4c2a339f0
Dhawal Majithia <deviolxd@icloud.com>
2019-01-13 22:37:50 -0800

    callback changes

--- htdocs/fb-callback.php
+++ htdocs/fb-callback.php
@@ -2,6 +2,44 @@
 
 require_once 'functions.php';
 
+$fb = new Facebook\Facebook([
+  'app_id' => $app_id, // Replace {app-id} with your app id
+  'app_secret' => $app_secret,
+  'default_graph_version' => 'v2.2',
+  ]);
+
+$helper = $fb->getRedirectLoginHelper();
+
+try {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+if (! isset($accessToken)) {
+  if ($helper->getError()) {
+    header('HTTP/1.0 401 Unauthorized');
+    echo "Error: " . $helper->getError() . "\n";
+    echo "Error Code: " . $helper->getErrorCode() . "\n";
+    echo "Error Reason: " . $helper->getErrorReason() . "\n";
+    echo "Error Description: " . $helper->getErrorDescription() . "\n";
+  } else {
+    header('HTTP/1.0 400 Bad Request');
+    echo 'Bad request';
+  }
+  exit;
+}
+
+// Logged in
+echo '<h3>Access Token</h3>';
+var_dump($accessToken->getValue());
+
 echo "hello";
 
 
