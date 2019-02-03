21d698f75b07ed6def54fb65eb87cd9fe68a5c3b
Dhawal Majithia <deviolxd@icloud.com>
2019-02-03 15:03:09 -0800

    [SERVER] cleaned up index.php and functions.php. Added functions - checkChromeID(), initDatabase().

--- htdocs/functions.php
+++ htdocs/functions.php
@@ -10,6 +10,48 @@ header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
 
+function checkChromeID($chromeID){
+	$conn = initDatabase();
+	$sql = "SELECT FacebookID, Name FROM users WHERE ChromeID = {$chromeID}";
+	$result = mysqli_query($conn, $sql);
+	$user['found'] = false; //assuming user not found
+	while($row = mysqli_fetch_assoc($result)){
+		//we found a user
+		$user['found'] = true;
+		$user['Name'] = $row['Name'];
+		$user['FacebookID'] = $row['FacebookID'];
+	}
+	$conn->close();
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	respond($user);
+}
+
+function initDatabase(){ //returns $conn from 'mysqli_connect()''
+	$servername = "localhost";
+	$username = "root";
+	$password = "grzvE3zXstWz";
+	$database = 'netflix_database';
+
+	// Create connection
+	//echo "about to create connection\n";
+	$conn = mysqli_connect($servername, $username, $password, $database);
+	return $conn;
+}
+
+function respond($value = {}){ //send back response to API caller
+	$response = array(
+			'type' => 'message',
+			'value' => $value
+	);
+	$response['request'] = json_decode(stripslashes($_REQUEST['json']), true);
+	$encoded = json_encode($response);
+	header('Conntent-type: application/json');
+	exit($encoded);
+}
+
 /*
 constants.php contains - 
 	$app_id,
--- popup.js
+++ popup.js
@@ -7,21 +7,21 @@ function initializeProgram(){
 	facebookLoginDiv.style.display = "none";
 	friendsFeedDiv.style.display = "none";
 
-	userIDCheck();
+	chromeIDCheck();
 }
 
-function userIDCheck(){
+function chromeIDCheck(){
 	chrome.identity.getProfileUserInfo(function(userInfo) {
 	  //document.getElementById("temp1").innerHTML = JSON.stringify(userInfo);
-		chrome.storage.sync.get({userIDs:{}}, function(result){
-			var userIDs = result.userIDs;
-			if (userInfo.id in userIDs){
+		chrome.storage.sync.get({chromeIDs:{}}, function(result){
+			var chromeIDs = result.chromeIDs;
+			if (userInfo.id in chromeIDs){
 				//get the friendsfeed from database
 				console.log("user exists in chrome.storage");
 			}
 			else {
 				console.log("user doesn't exist in chrome.storage");
-				userIDCheckApi(userInfo.id, function(response){
+				chromeIDCheckApi(userInfo.id, function(response){
 					if(response.found){
 						console.log("user found in database");
 						//get the friends feed from database
@@ -38,7 +38,7 @@ function userIDCheck(){
 	});
 }
 
-function userIDCheckApi(chromeID, callback){
+function chromeIDCheckApi(chromeID, callback){
 	const Url = "https://54.186.219.119?action=checkChromeID&chromeID=" + chromeID;
 	fetch(Url, {method: "GET"})
 	.then(function(data){
